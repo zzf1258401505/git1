d491525d914e28a5ae9ed79ce5bfd949e334f0d3
zhang <zhang@163.com>
2022-10-22 09:52:01 +0800

    first commit

---
+++ src/main/java/com/zhang/App.java
@@ -0,0 +1,15 @@
+package com.zhang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello git!" );
+        System.out.println( "Hello git!" );
+    }
+}
